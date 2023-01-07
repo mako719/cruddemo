7ab94a621ccd00e6d0a16764d60ef1d396fe6ee6
mako719 <shoki.sm.sk@gmail.com>
2023-01-07 17:19:38 +0900

    不要なコードを削除

--- src/main/java/com/raisetech/cruddemo/Team.java
+++ src/main/java/com/raisetech/cruddemo/Team.java
@@ -2,10 +2,8 @@ package com.raisetech.cruddemo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class Team {
     private int id;
